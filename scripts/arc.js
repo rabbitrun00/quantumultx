93c73e47eba1721f6aa562c8319dc54912223f36
rabbitrun00 <105692542+rabbitrun00@users.noreply.github.com>
2022-11-17 19:16:29 +0800

    Update arc.js

--- scripts/arc.js
+++ scripts/arc.js
@@ -1,6 +1,6 @@
 [rewrite_local]
 # > arc app
-^https?:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/rabbitrun00/quantumultx/main/scripts/arc.js
+^https?:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://github.com/rabbitrun00/quantumultx/blob/main/scripts/arc.js
 
 
 [mitm] 
