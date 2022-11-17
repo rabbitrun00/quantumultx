097d3665e946cc45ec409b7b94a26baa8afe3013
rabbitrun00 <105692542+rabbitrun00@users.noreply.github.com>
2022-11-17 19:00:51 +0800

    Create arc.js

---
+++ scripts/arc.js
@@ -0,0 +1,50 @@
+[rewrite_local]
+# > arc app
+^https?:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/rabbitrun00/quau/main/arc.js
+
+[mitm] 
+hostname = api.revenuecat.com
+
+*******************************/
+
+{
+  "request_date_ms" : 1668626565540,
+  "request_date" : "2022-11-16T19:22:45Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2022-11-13T18:53:04Z",
+    "original_application_version" : "370003",
+    "other_purchases" : {
+
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "subscriptionBiannual" : {
+        "original_purchase_date" : "2022-11-13T18:57:33Z",
+        "expires_date" : "2023-01-13T18:57:32Z",
+        "is_sandbox" : false,
+        "refunded_at" : null,
+        "unsubscribe_detected_at" : "2022-11-13T20:13:55Z",
+        "grace_period_expires_date" : null,
+        "period_type" : "trial",
+        "purchase_date" : "2022-11-13T18:57:32Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "auto_resume_date" : null
+      }
+    },
+    "entitlements" : {
+      "backer" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2022-11-13T18:57:32Z",
+        "product_identifier" : "subscriptionBiannual",
+        "expires_date" : "9999-01-13T18:57:32Z"
+      }
+    },
+    "original_purchase_date" : "2022-11-13T18:52:45Z",
+    "original_app_user_id" : "$RCAnonymousID:a885a2e257664d028030254e7b36bd7e",
+    "last_seen" : "2022-11-16T09:04:15Z"
+  }
